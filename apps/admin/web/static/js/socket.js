763d68b899be731905129313ec5b792f3ce36a0c
Henrik Tenhovirta <henrik@flockler.com>
2015-10-20 09:27:08 +0300

    Typoed word again

--- apps/admin/web/static/js/socket.js
+++ apps/admin/web/static/js/socket.js
@@ -57,6 +57,6 @@ socket.connect({token: window.userToken})
 let channel = socket.channel("topic:subtopic", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined succesfully", resp) })
-  .receive("error", resp => { console.log("Unabled to join", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
 
 export default socket
